b0fb87284d6a413c4a82b6f60c14ac7321cdee59
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-07-10 21:31:02 +0900

    #1 [CHORE] Bottom Tabs 생성하기

--- App.tsx
+++ App.tsx
@@ -1,28 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import BookSearch from './src/screens/BookSearch';
+import Library from './src/screens/Library';
+import MyPage from './src/screens/MyPage';
+
+import { NavigationContainer } from '@react-navigation/native';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+    const Stack = createNativeStackNavigator();
+    const Tab = createBottomTabNavigator();
+    const BottomTabScreen = () => {
+        return (
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    headerShown: false,
+                    tabBarStyle: {
+                        height: 70,
+                    },
+                })}
+            >
+                <Tab.Screen name='BookSearch' component={BookSearch} />
+                <Tab.Screen name='Library' component={Library} />
+                <Tab.Screen name='MyPage' component={MyPage} />
+            </Tab.Navigator>
+        );
+    };
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name='Bottom' component={BottomTabScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
