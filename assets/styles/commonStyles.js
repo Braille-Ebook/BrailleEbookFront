89225119df4d3d6e67302c1c0a28ca7478c0daf8
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-07-16 12:25:56 +0900

    #3 [FEAT] LibaryScreen 구성하기

---
+++ assets/styles/commonStyles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const commonStyles = StyleSheet.create({
+    titleText: {
+        fontSize: 25,
+    },
+    subtitleText: {
+        fontSize: 16,
+    },
+});
+
+export default commonStyles;
---
+++ src/components/Book.js
@@ -0,0 +1,53 @@
+import { StyleSheet, View, Text, Image } from 'react-native';
+import React from 'react';
+import commonStyles from '../../assets/styles/commonStyles';
+
+const Book = ({ data }) => {
+    return (
+        <View style={styles.bookContainer}>
+            <Image
+                source={require('../../assets/images/littleRedRidingHood.png')}
+                style={styles.bookImage}
+            />
+            <View>
+                <Text style={commonStyles.subtitleText}>{data.title}</Text>
+                <Text>
+                    {data.translator
+                        ? `${data.author} 글, ${data.translator} 번역`
+                        : `${data.author} 글`}
+                </Text>
+                <Text>{`${data.publishDate.getFullYear()}.${
+                    data.publishDate.getMonth() + 1
+                }.${data.publishDate.getDate()}`}</Text>
+
+                <View style={styles.bookmarkContainer}>
+                    <Image
+                        source={
+                            data.isBookmarked
+                                ? require('../../assets/icons/bookmarkIconFill.png')
+                                : require('../../assets/icons/bookmarkIcon.png')
+                        }
+                    />
+                    <Text>{`북마크 ${data.bookmarkNum}`}</Text>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    bookContainer: {
+        flexDirection: 'row',
+        paddingHorizontal: 36,
+        marginBottom: 10,
+    },
+    bookImage: {
+        marginRight: 15,
+    },
+    bookmarkContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+});
+
+export default Book;
